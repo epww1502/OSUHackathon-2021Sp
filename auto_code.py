67b7ae58cb48271d5bda7e8b7f44f51e4e2da94d
epww1502 <43550999+epww1502@users.noreply.github.com>
2021-04-09 23:16:09 -0700

    Updated auto_code.py
    Combined api.py and strategy.py

---
+++ auto_code.py
@@ -0,0 +1,124 @@
+############################ API ##########################
+import pybithumb # For bithumb
+import time
+import requests # For slack
+import datetime
+
+# Read connect key and secret key from upbit.txt (first line: connect key / second: secret key)
+with open("bithumb.txt") as f:
+    lines = f.readlines()
+    key = lines[0].strip()
+    secret = lines[1].strip()
+    bithumb = pybithumb.Bithumb(key, secret)
+
+ 
+def post_message(token, channel, text):
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text}
+    )
+    print(response)
+ 
+slack = "xoxb-~~~~" # Should input slack info
+post_message(slack,"#stock","Connected to Slack")
+
+## Balance Checking
+def balance_check():
+    post_message(slack,"#stock","Current Balance: \n")
+    for ticker in pybithumb.get_tickers() :
+        balance = bithumb.get_balance(ticker)
+        print(ticker, ":", balance)
+        post_message(slack,"#stock", ticker + ":" + balance + " ")
+        time.sleep(0.1)
+
+    # Method 2
+    '''
+    balances = bithumb.get_balances()
+    for balance in balances:
+        print(balance)
+        post_message(slack,"#stock", balance)
+    '''
+
+    # Method 3
+    '''
+    balances = bithumb.get_balances()
+    for i in range(0,34):
+        print(i, balances[i]['currency'], balances[i]['balance'])
+        post_message(slack,"#stock", i + " " + balances[i]['currency'] + " " + balances[i]['balance'])
+    '''
+
+balance_check()
+
+
+## Buying cryptocurrency -- DO NOT RUN THIS CODE!!!!!!!!!!!!!
+'''
+ret = bithumb.buy_limit_order("KRW-XRP", 100, 20)
+print(ret)
+'''
+
+## Selling cryptocurrency -- DO NOT RUN THIS CODE!!!!!!!!!!!!!
+'''
+ret = bithumb.sell_limit_order("KRW-XRP", 1000, 20)
+print(ret)
+'''
+
+## Cancel Order
+'''
+ret = bithumb.cancel_order('cc52be46-1000-4126-aee7-9bfafb867682')
+print(ret)
+'''
+
+
+
+
+######################### Volatility_breakout.py ##############################
+def get_target_price(ticker):
+    df = pybithumb.get_ohlcv(ticker)
+    yesterday = df.iloc[-2]
+
+    today_open = yesterday['close']
+    yesterday_high = yesterday['high']
+    yesterday_low = yesterday['low']
+    target = today_open + (yesterday_high - yesterday_low) * 0.5
+    return target
+
+def get_yesterday_ma5(ticker):
+    df = pybithumb.get_ohlcv(ticker)
+    close = df['close']
+    ma = close.rolling(5).mean()
+    return ma[-2]
+
+
+def buy_crypto_currency(ticker):
+    krw = bithumb.get_balance(ticker)[2] # "upbit" from api.py
+    orderbook = pybithumb.get_orderbook(ticker)
+    sell_price = orderbook['asks'][0]['price']   
+    unit = krw/float(sell_price)
+    buy_bit = bithumb.buy_market_order(ticker, unit) # {'uuid': '0182cc61-d1a6-4827-9505-74f60a8b076c', 'side': 'bid', 'ord_type': 'price', 'price': '10000.0', 'state': ', 'remaining_fee': wait', 'market': 'KRW-BTC', 'created_at': '2021-02-08T11:05:47+09:00', 'volume': None, 'remaining_volume': None, 'reserved_fee': '5.0', 'remaining_fee': '5.0', 'paid_fee': '0.0', 'locked': '10005.0', 'executed_volume': '0.0', 'trades_count': 0}
+    # post_message(slack,"#stock", "BUY " + buy_bit['market'] + " / Price: " + buy_bit['price'] + " / Time: " + buy_bit['created_at'])
+
+
+def sell_crypto_currency(ticker):
+    unit = bithumb.get_balance(ticker)[0]
+    sell_bit = bithumb.sell_market_order(ticker, unit)
+    # post_message(slack,"#stock", "SELL " + sell_bit['market'] + " / Price: " + sell_bit['price'] + " / Time: " + sell_bit['created_at'])
+
+now = datetime.datetime.now()
+mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+target_price = get_target_price("BTC")
+ma5 = get_yesterday_ma5("BTC")
+
+while True:
+    try:
+        now = datetime.datetime.now()
+        if mid < now < mid + datetime.delta(seconds=10): 
+            target_price = get_target_price("BTC")
+            mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+            sell_crypto_currency("BTC")
+
+        current_price = pybithumb.get_current_price("BTC")
+        if (current_price > target_price) and (current_price > ma5):
+            buy_crypto_currency("BTC")
+    except:
+        print("Error Occured")        
+    time.sleep(1)
--- test.py
+++ test.py
@@ -1,5 +1,8 @@
+import pybithumb
+import numpy as np
 import requests
- 
+
+'''
 def post_message(token, channel, text):
     response = requests.post("https://slack.com/api/chat.postMessage",
         headers={"Authorization": "Bearer "+token},
@@ -10,3 +13,64 @@ def post_message(token, channel, text):
 myToken = "xoxb-1940285825253-1967173782768-aWN7zF405rIpqnBG9PA4Syvr"
  
 post_message(myToken,"#stock","testing code")
+'''
+
+# 전체 기간 (2013 ~ Current) 백테스팅 코드
+df = pybithumb.get_ohlcv("BTC")
+
+df['ma5'] = df['close'].rolling(window=5).mean().shift(1)
+df['range'] = (df['high'] - df['low']) * 0.5
+# df['range_shift1'] = df['range'].shift(1)
+df['target'] = df['open'] + df['range'].shift(1)
+df['bull'] = df['open'] > df['ma5']
+
+fee = 0.0032 # 수수료
+df['ror'] = np.where(df['high'] > df['target'], df['close'] / df['target'] - fee, 1)
+
+# 거래일별로 기간수익률
+df['hpr'] = df['ror'].cumprod()
+
+# MDD(Maximum Draw Down)은 투자 기간 중에 포트폴리오의 전 고점에서 저점까지의 최대 누적 손실을 의미합니다. 
+# dd = Drawdown
+df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100 
+
+print(df.tail())
+
+print("MDD: ", df['dd'].max())
+print("HPR: ", df['hpr'][-2])
+
+df.to_csv("btc_ma.csv")
+
+
+
+# 특정 년도 백테스팅 코드 (e.g. 2020)
+'''
+def get_hpr(ticker):
+    try:
+        df = pybithumb.get_ohlcv(ticker)
+        df = df['2020']
+
+        df['ma5'] = df['close'].rolling(window=5).mean().shift(1)
+        df['range'] = (df['high'] - df['low']) * 0.5
+        df['target'] = df['open'] + df['range'].shift(1)
+        df['bull'] = df['open'] > df['ma5']
+
+        fee = 0.0032
+        df['ror'] = np.where((df['high'] > df['target']) & df['bull'], df['close'] / df['target'] - fee, 1)
+
+        df['hpr'] = df['ror'].cumprod()
+        df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100
+        return df['hpr'][-2]
+    except:
+        return 1
+
+tickers = pybithumb.get_tickers()
+
+hprs = []
+for ticker in tickers:
+    hpr = get_hpr(ticker)
+    hprs.append((ticker, hpr))
+
+sorted_hprs = sorted(hprs, key=lambda x:x[1], reverse=True)
+print(sorted_hprs[:5]) # 기간수익률이 높은 5개의 코인 정보를 화면에 출력
+'''
