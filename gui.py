15576c07c863f34897acef0d9108e28a9bb23e27
wnsgur4322 <32208343+wnsgur4322@users.noreply.github.com>
2021-04-09 23:50:01 -0700

    market notification

--- gui.py
+++ gui.py
@@ -3,7 +3,9 @@ from PyQt5 import QtWidgets
 from PyQt5 import QtGui
 from PyQt5 import uic
 from PyQt5 import QtCore
+import pyupbit
 
+tickers = ["KRW-BTC", "KRW-ETH", "KRW-BCH", "KRW-ETC"]
 form_class = uic.loadUiType("bull.ui")[0]
 
 class MyWindow(QtWidgets.QMainWindow, form_class):
@@ -12,6 +14,7 @@ class MyWindow(QtWidgets.QMainWindow, form_class):
                 self.setGeometry(800,300,400,400)
                 self.setWindowIcon(QtGui.QIcon(".\image\kimchicoin.png"))
                 self.setupUi(self)
+                self.tableWidget.setRowCount(len(tickers))
                 
                 btn = QtWidgets.QPushButton("btn 1", self)
                 btn.move(10,10)
@@ -26,8 +29,29 @@ class MyWindow(QtWidgets.QMainWindow, form_class):
                 timer.start(5000)
                 timer.timeout.connect(self.timeout)
 
+        def get_market_infos(self, ticker):
+                df = pyupbit.get_ohlcv(ticker)
+                ma5 = df['close'].rolling(window=5).mean()
+                last_ma5 = ma5[-2]
+                price = pyupbit.get_current_price(ticker)
+ 
+                state = None
+                if price > last_ma5:
+                        state = "bull"
+                else:
+                        state = "bear"
+
+                return price, last_ma5, state
+
         def timeout(self):
-                print("it is bull market! time to sell!")
+                for i, ticker in enumerate(tickers):
+                        item = QtWidgets.QTableWidgetItem(ticker)
+                        self.tableWidget.setItem(i, 0, item)
+ 
+                        price, last_ma5, state = self.get_market_infos(ticker)
+                        self.tableWidget.setItem(i, 1, QtWidgets.QTableWidgetItem(str(price)))
+                        self.tableWidget.setItem(i, 2, QtWidgets.QTableWidgetItem(str(last_ma5)))
+                        self.tableWidget.setItem(i, 3, QtWidgets.QTableWidgetItem(state))
 
         def btn_clicked(self):
                 print("btn clikced!")
@@ -36,6 +60,7 @@ app = QtWidgets.QApplication(sys.argv)
 window = MyWindow()
 window.show()
 app.exec_()
+print(pyupbit.get_tickers(fiat="KRW"))
 # btn = QtWidgets.QPushButton("bitcoin!")
 # btn.show()
 # label = QtWidgets.QLabel("Hello")
